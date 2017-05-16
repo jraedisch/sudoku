69eb9a14bcb151a0e72711c039df4226bf6e6599
Jasper Rädisch <jasper.raedisch@gmail.com>
2017-05-16 22:16:48 +0200

    simple solver and backtrack solver

--- sudoku.go
+++ sudoku.go
@@ -2,7 +2,8 @@
 package sudoku
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"math"
 	"strconv"
 )
@@ -10,20 +11,46 @@ import (
 // Board contains all fields of a simple, unannotated sudoku.
 type Board [][]int
 
-// AnnotatedBoard contains one slice per field to hold possible "penciled" candidates.
-type AnnotatedBoard [][]Candidates
-
-// Candidates contains all "penciled" numbers that may occupy a field.
-type Candidates int
-
-// allCandidates returns all candidates for a field of a sudoku with provided size.
-func allCandidates(size int) (c Candidates) {
-	for size > 0 {
-		c = c.Add(size)
-		size--
+// Copy returns a copy of the board. Helpful to stay as immutible as possible for now.
+func (bo Board) Copy() (bo2 Board) {
+	size := len(bo)
+	bo2 = make(Board, size)
+	for i := range bo {
+		bo2[i] = make([]int, size)
+		copy(bo2[i], bo[i])
 	}
 	return
 }
+
+// FirstEmpty returns coordinates of first empty field and whether one was found.
+func (bo Board) FirstEmpty() (y, x int, found bool) {
+	for y = range bo {
+		for x = range bo[y] {
+			if bo[y][x] == 0 {
+				found = true
+				return
+			}
+		}
+	}
+	return
+}
+
+const abc = "abcdefghijklmnopqrstuvwxyz"
+
+// Short returns simple sudoku string representation, e.g. "a18b52".
+func (bo Board) Short() (s string) {
+	for y, row := range bo {
+		for x, v := range row {
+			if v > 0 {
+				s += string(abc[x]) + strconv.Itoa(y+1) + strconv.Itoa(v)
+			}
+		}
+	}
+	return
+}
+
+// Candidates contains all "penciled" numbers that may occupy a field.
+type Candidates int
 
 // Add adds provided number to candidates (if not exists).
 func (c Candidates) Add(v int) Candidates {
@@ -40,68 +67,13 @@ func (c Candidates) Contains(v int) bool {
 	return c&(1<<uint(v)) != 0
 }
 
-func newAnnotatedBoard(size int) (ab AnnotatedBoard) {
-	ab = make(AnnotatedBoard, size)
-	for y := range ab {
-		ab[y] = make([]Candidates, size)
-	}
-	return ab
+// Single returns whether candidates contain a single candidate.
+func (c Candidates) Single() bool {
+	// Check if c is a power of two.
+	return (c > 0 && ((c & (c - 1)) == 0))
 }
 
-// Solvable checks whether a board is still solvable (also true if solved correctly).
-func (bo Board) Solvable() bool {
-	return false
-}
-
-// Annotate (naively) a board with possible candidates for each field.
-func (bo Board) Annotate() (ab AnnotatedBoard) {
-	le := len(bo[0])
-	rt := sqrt(le)
-	rows, cols := make([]Candidates, le), make([]Candidates, le)
-	blocks := newAnnotatedBoard(rt)
-	ab = newAnnotatedBoard(le)
-	for y, row := range bo {
-		for x, v := range row {
-			if v > 0 {
-				ab[y][x] = ab[y][x].Add(v)
-				rows[y] = rows[y].Add(v)
-				cols[x] = cols[x].Add(v)
-				blocks[y/rt][x/rt] = blocks[y/rt][x/rt].Add(v)
-			}
-		}
-	}
-	for y, row := range ab {
-		for x, v := range row {
-			if v > 1 {
-				continue
-			}
-			ab[y][x] = allCandidates(le) &^ rows[y] &^ cols[x] &^ blocks[y/rt][x/rt]
-		}
-	}
-	return ab
-}
-
-// A Parser is a function that parses a certain string representation of a sudoku into a board.
-type Parser func(unparsed string) (bo Board, err error)
-
-// A Solver is a function that tries to solve an annotated board following a certain strategy.
-// An integer should be provided to limit the amount of calculated solutions.
-// -1 means, as many solutions as possible will be calculated.
-type Solver func(ab AnnotatedBoard, maxSolutions int) (solved bool, solutions []Board)
-
-// SingleCandidate will look through annotated board for fields with just one candidate, fill it, recalculate candidates for all fields and repeat.
-func SingleCandidate(ab AnnotatedBoard, maxSolutions int) (solved bool, solutions []Board) {
-	return
-}
-
-// Backtrack implements a simple backtracking solver.
-func Backtrack(ab AnnotatedBoard, maxSolutions int) (solved bool, solutions []Board) {
-	return false, nil
-}
-
-// Helpers
-
-// Decimals returns candidates as a more readable decimal array.
+// Decimals returns candidates as a decimal array.
 func (c Candidates) Decimals() (dcs []int) {
 	i := int(c)
 	for max := log2(i); max > 0; max-- {
@@ -112,6 +84,139 @@ func (c Candidates) Decimals() (dcs []int) {
 		}
 	}
 	return
+}
+
+// AnnotatedBoard enriches a board with various annotation ("penceling") helpers.
+type AnnotatedBoard struct {
+	Board
+	Candidates, Blocks [][]Candidates
+	Rows, Cols         []Candidates
+}
+
+// NewAnnotatedBoard returns an annotated version of provided board.
+func NewAnnotatedBoard(bo Board) (ab AnnotatedBoard, err error) {
+	ab = AnnotatedBoard{Board: bo.Copy()}
+	return ab.Annotate()
+}
+
+// Solved returns true if board is solved correctly.
+func (ab AnnotatedBoard) Solved() bool {
+	for _, row := range ab.Candidates {
+		for _, v := range row {
+			if !v.Single() {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// Annotate (naively) annotates a board with possible candidates for each field.
+// All data except board will be overwritten.
+func (ab AnnotatedBoard) Annotate() (AnnotatedBoard, error) {
+	ab.Board = ab.Board.Copy()
+	le := len(ab.Board[0])
+	rt := sqrt(le)
+	ab.Rows, ab.Cols = make([]Candidates, le), make([]Candidates, le)
+	ab.Candidates, ab.Blocks = newBlockCandidates(le), newBlockCandidates(rt)
+	for y, row := range ab.Board {
+		for x, v := range row {
+			if v > 0 {
+				if ab.Rows[y].Contains(v) || ab.Cols[x].Contains(v) || ab.Blocks[y/rt][x/rt].Contains(v) {
+					return ab, errors.New("Not Solvable.")
+				}
+				ab.Candidates[y][x] = ab.Candidates[y][x].Add(v)
+				ab.Rows[y] = ab.Rows[y].Add(v)
+				ab.Cols[x] = ab.Cols[x].Add(v)
+				ab.Blocks[y/rt][x/rt] = ab.Blocks[y/rt][x/rt].Add(v)
+			}
+		}
+	}
+	for y, row := range ab.Candidates {
+		for x, v := range row {
+			if v > 1 {
+				continue
+			}
+			ab.Candidates[y][x] = allCandidates(le) &^ ab.Rows[y] &^ ab.Cols[x] &^ ab.Blocks[y/rt][x/rt]
+		}
+	}
+	return ab, nil
+}
+
+// A Parser is a function that parses a certain string representation of a sudoku into a board.
+type Parser func(unparsed string) (bo Board, err error)
+
+// A Solver is a function that tries to solve an annotated board following a certain strategy.
+// An integer should be provided to limit the amount of calculated solutions.
+// -1 means, as many solutions as possible will be calculated.
+type Solver func(ab AnnotatedBoard, maxSolutions int) (solved bool, solutions []Board)
+
+// SingleCandidate will look through annotated board for fields with just one candidate, fill it, recalculate candidates for all fields, and repeat, until board is solved or no longer solvable.
+// It will not return more than one solution.
+func SingleCandidate(ab AnnotatedBoard, maxSolutions int) (bool, []Board) {
+	solvable := true
+	ab.Board = ab.Copy()
+
+	for solvable {
+		solvable = false
+		for y, row := range ab.Candidates {
+			for x, c := range row {
+				if !c.Single() || ab.Board[y][x] > 0 {
+					continue
+				}
+
+				ab.Board[y][x] = c.Decimals()[0]
+				solvable = true
+			}
+		}
+		ab, _ = ab.Annotate()
+	}
+	return ab.Solved(), []Board{ab.Board}
+}
+
+// Backtrack implements a simple backtracking solver. It is not performant but guaranteed to finish.
+func Backtrack(ab AnnotatedBoard, maxSolutions int) (solved bool, solutions []Board) {
+	solutions = []Board{}
+	return backtrack(ab, maxSolutions, &solutions), solutions
+}
+
+func backtrack(ab AnnotatedBoard, maxSolutions int, solutions *[]Board) bool {
+	var err error
+	ab, err = ab.Annotate()
+	if err != nil {
+		return false
+	}
+	y, x, found := ab.Board.FirstEmpty()
+	if !found {
+		*solutions = append(*solutions, ab.Board)
+		return len(*solutions) >= maxSolutions
+	}
+	for _, v := range ab.Candidates[y][x].Decimals() {
+		ab.Board[y][x] = v
+		if backtrack(ab, maxSolutions, solutions) {
+			return true
+		}
+	}
+	return false
+}
+
+// Helpers
+
+// allCandidates returns all candidates for a field of a sudoku with provided size.
+func allCandidates(size int) (c Candidates) {
+	for size > 0 {
+		c = c.Add(size)
+		size--
+	}
+	return
+}
+
+func newBlockCandidates(size int) (bc [][]Candidates) {
+	bc = make([][]Candidates, size)
+	for y := range bc {
+		bc[y] = make([]Candidates, size)
+	}
+	return bc
 }
 
 func sqrt(i int) int {
@@ -131,5 +236,5 @@ func b(i Candidates) string {
 }
 
 func l(vs ...interface{}) {
-	fmt.Print(vs...)
+	log.Print(vs...)
 }
--- sudoku_test.go
+++ sudoku_test.go
@@ -12,24 +12,117 @@ var solved4 = Board{
 	{2, 3, 4, 1},
 }
 
-var empty4 = Board{
+var unsolved4 = Board{
 	{1, 0, 0, 0},
 	{0, 2, 0, 0},
 	{0, 0, 3, 0},
 	{0, 0, 0, 4},
 }
 
-var annotated4 = AnnotatedBoard{
+var unsolved4b = Board{
+	{1, 2, 0, 0},
+	{3, 0, 0, 2},
+	{4, 1, 2, 0},
+	{2, 3, 4, 1},
+}
+
+var unsolved4c = Board{
+	{0, 0, 0, 0},
+	{0, 0, 0, 0},
+	{0, 0, 0, 0},
+	{0, 0, 0, 0},
+}
+
+var unsolved9 = Board{
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0},
+}
+
+var solved9 = Board{
+	{9, 8, 7, 6, 5, 4, 3, 2, 1},
+	{6, 5, 4, 3, 2, 1, 9, 8, 7},
+	{3, 2, 1, 9, 8, 7, 6, 5, 4},
+	{8, 9, 6, 7, 4, 5, 2, 1, 3},
+	{7, 4, 5, 2, 1, 3, 8, 9, 6},
+	{2, 1, 3, 8, 9, 6, 7, 4, 5},
+	{5, 7, 9, 4, 6, 8, 1, 3, 2},
+	{4, 6, 8, 1, 3, 2, 5, 7, 9},
+	{1, 3, 2, 5, 7, 9, 4, 6, 8},
+}
+
+var annotated4 = [][]Candidates{
 	{2, 24, 20, 12},
 	{24, 4, 18, 10},
 	{20, 18, 8, 6},
 	{12, 10, 6, 16},
 }
 
+func TestFirstEmpty(t *testing.T) {
+	y, x, found := unsolved4b.FirstEmpty()
+	if !found {
+		t.Error("Expected value to be found.")
+	}
+	if y != 0 {
+		t.Errorf("Expected 0, got %d", y)
+	}
+	if x != 2 {
+		t.Errorf("Expected 2, got %d", x)
+	}
+	_, _, found = solved4.FirstEmpty()
+	if found {
+		t.Error("Expected value not to be found.")
+	}
+}
+
+func TestBacktrack(t *testing.T) {
+	annotated, _ := NewAnnotatedBoard(unsolved9)
+	solved, solutions := Backtrack(annotated, 4)
+	if !solved {
+		t.Error("Expected board to be solved.")
+	}
+	if len(solutions) != 4 {
+		t.Errorf("Expected 4 solutions, got %d", len(solutions))
+	}
+	if !reflect.DeepEqual(solved9, solutions[0]) {
+		t.Errorf("Expected %+v, got %+v", solved9, solutions[0])
+	}
+}
+
+func TestSingleCandidate(t *testing.T) {
+	expectedSolutions := []Board{solved4}
+	annotated, _ := NewAnnotatedBoard(unsolved4b)
+	solved, solutions := SingleCandidate(annotated, 1)
+	if !solved {
+		t.Error("Expected board to be solved.")
+	}
+	if !reflect.DeepEqual(expectedSolutions, solutions) {
+		t.Errorf("Expected %+v, got %+v", expectedSolutions, solutions)
+	}
+}
+
+func TestSingleCandidateUnsolvable(t *testing.T) {
+	expectedSolutions := []Board{unsolved4}
+	annotated, _ := NewAnnotatedBoard(unsolved4)
+	solved, solutions := SingleCandidate(annotated, 1)
+	if solved {
+		t.Error("Expected board not to be solved.")
+	}
+	if !reflect.DeepEqual(expectedSolutions, solutions) {
+		t.Errorf("Expected %+v, got %+v", expectedSolutions, solutions)
+	}
+}
+
 func TestAnnotate(t *testing.T) {
-	actual := empty4.Annotate()
-	if !reflect.DeepEqual(annotated4, actual) {
-		t.Errorf("Expected %v, got %v", annotated4, actual)
+	actual, _ := NewAnnotatedBoard(unsolved4)
+	if !reflect.DeepEqual(annotated4, actual.Candidates) {
+		t.Errorf("Expected %v, got %v", annotated4, actual.Candidates)
 	}
 }
 
@@ -46,13 +139,22 @@ func TestCandidates(t *testing.T) {
 	if "0" != b(c) {
 		t.Errorf(`Expected "0", got "%s"`, b(c))
 	}
+	if c.Single() {
+		t.Errorf("Expected %+v not to be single.", c)
+	}
 	c = c.Add(9)
 	if "1000000000" != b(c) {
 		t.Errorf(`Expected "1000000000", got "%s".`, b(c))
 	}
+	if !c.Single() {
+		t.Errorf("Expected %+v to be single.", c)
+	}
 	c = c.Add(1)
 	if "1000000010" != b(c) {
 		t.Errorf(`Expected "1000000010", got "%s".`, b(c))
+	}
+	if c.Single() {
+		t.Errorf("Expected %+v not to be single.", c)
 	}
 	c = c.Subtract(7)
 	if "1000000010" != b(c) {
@@ -74,17 +176,13 @@ func TestCandidates(t *testing.T) {
 	}
 }
 
-func TestNewAnnotatedBoard(t *testing.T) {
-	expected := AnnotatedBoard{
+func TestNewBlockCandidates(t *testing.T) {
+	expected := [][]Candidates{
 		make([]Candidates, 2),
 		make([]Candidates, 2),
 	}
-	actual := newAnnotatedBoard(2)
+	actual := newBlockCandidates(2)
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
-}
-
-func TestBacktrack(t *testing.T) {
-
 }
